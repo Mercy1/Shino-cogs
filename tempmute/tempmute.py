13e7596146d82f13daa79bb741d2af409e14ce07
DannyDB5544 <59149717+DannyDB5544@users.noreply.github.com>
2020-06-30 21:28:29 +0100

    Update tempmute.py

--- tempmute/tempmute.py
+++ tempmute/tempmute.py
@@ -4,7 +4,7 @@ from datetime import datetime, timedelta
 from typing import Optional
 
 import discord
-from redbot.cogs import ModEX as Mod
+from redbot.cogs.modEX import ModEX as Mod
 from redbot.core import Config, checks, commands, modlog
 from redbot.core.commands.converter import TimedeltaConverter
 from redbot.core.utils.chat_formatting import humanize_list, humanize_timedelta
