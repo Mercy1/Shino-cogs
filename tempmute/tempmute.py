3f2f6c4983d8b02adbe027801224ba750d561ece
Mercy1 <taylorangel887@gmail.com>
2020-07-14 16:10:45 +0100

    Removed context

--- tempmute/tempmute.py
+++ tempmute/tempmute.py
@@ -19,10 +19,6 @@ class tempmute(tempmute):
 
     __version__ = "1.1.4"
 
-    def format_help_for_context(self, ctx):
-        """Thanks Sinbad."""
-        pre_processed = super().format_help_for_context(ctx)
-        return f"{pre_processed}\nCog Version: {self.__version__}"
 
     def __init__(self, bot):
         super().__init__(bot)
