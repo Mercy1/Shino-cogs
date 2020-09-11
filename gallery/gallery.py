f9f2ecf6953f9f1d841d8e163fd8a9a5df312f15
Mercy1 <taylorangel887@gmail.com>
2020-09-11 18:10:07 +0100

    Fixed spoiler issue
    idk why the bot dislikes Discord features, you know with all the rate-limiting and that, I really don't see why.... xD

--- gallery/gallery.py
+++ gallery/gallery.py
@@ -107,7 +107,7 @@ class Gallery(commands.Cog):
             return
         if not message.attachments:
             uris = re.findall(
-                "(?=\S+youtube\.com|\S+youtu\.be\/|\S+tenor\.com|\S+\.mov|\S+\.jpg|\S+\.jpeg|\S+\.tiff|\S+\.gif|\S+\.bmp|\S+\.mp4|\S+\.webm|\S+\.png)((?<!\S)(((f|ht){1}tp[s]?:\/\/|(?<!\S)www\.)[-a-zA-Z0-9@:%_\+.~#?&\/\/=]+))",
+                "(?=\S+youtube\.com|\S+youtu\.be\/|\S+tenor\.com|\S+\.mov|\S+\.jpg|\S+\.jpeg|\S+\.tiff|\S+\.gif|\S+\.bmp|\S+\.mp4|\S+\.webm|\S+\.png|)((?<!\S)(((f|ht){1}tp[s]?:\/\/|[\|\|]+|(?<!\S)www\.)[-a-zA-Z0-9@:%_\+.~#?&|\/\/=]+))",
                 message.content,
             )
             if len(uris) == 1:
