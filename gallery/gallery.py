4d76d2388dbfc1fc613adf3794cccfe9d10d66d1
DannyDB5544 <59149717+DannyDB5544@users.noreply.github.com>
2020-09-29 22:30:28 +0100

    Update gallery.py

--- gallery/gallery.py
+++ gallery/gallery.py
@@ -109,10 +109,9 @@ class Gallery(commands.Cog):
         if not message.attachments:
             escaped = discord.utils.escape_markdown(message.content)
             await message.channel.send(message.content)
-            stripped = re.sub("\\Q|\\E", "", escaped)  
             uris = re.findall(
                 "(?=\S+youtube\.com|\S+youtu\.be\/|\S+tenor\.com|\S+\.mov|\S+\.jpg|\S+\.jpeg|\S+\.tiff|\S+\.gif|\S+\.bmp|\S+\.mp4|\S+\.webm|\S+\.png|)((?<!\S)(((f|ht){1}tp[s]?:\/\/+|(?<!\S)www\.)[-a-zA-Z0-9@:%_\+.~#?&|\/\/=]+))",
-                stripped,
+                escaped,
             )
             if len(uris) == 1:
                 uri = "".join(uris)
