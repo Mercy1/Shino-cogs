71bd6cd1ee9b7c2be6e5fe65aecf11af428e320c
DannyDB5544 <59149717+DannyDB5544@users.noreply.github.com>
2020-09-29 22:12:18 +0100

    Update gallery.py

--- gallery/gallery.py
+++ gallery/gallery.py
@@ -108,8 +108,9 @@ class Gallery(commands.Cog):
             return
         if not message.attachments:
             escaped = common_filters.escape_spoilers(message.content)
-            await message.channel.send(f"{dab}")
+            await message.channel.send(f"{escaped}")
             stripped = escaped.lstrip("|")
+            await message.channel.send(f"{stripped}")
             uris = re.findall(
                 "(?=\S+youtube\.com|\S+youtu\.be\/|\S+tenor\.com|\S+\.mov|\S+\.jpg|\S+\.jpeg|\S+\.tiff|\S+\.gif|\S+\.bmp|\S+\.mp4|\S+\.webm|\S+\.png|)((?<!\S)(((f|ht){1}tp[s]?:\/\/+|(?<!\S)www\.)[-a-zA-Z0-9@:%_\+.~#?&|\/\/=]+))",
                 dab,
