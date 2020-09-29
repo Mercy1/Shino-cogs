cffc5066321011390a1477c50497fb9e388f48ac
DannyDB5544 <59149717+DannyDB5544@users.noreply.github.com>
2020-09-29 22:27:28 +0100

    Update gallery.py

--- gallery/gallery.py
+++ gallery/gallery.py
@@ -107,7 +107,7 @@ class Gallery(commands.Cog):
         if message.channel.id not in await self.config.guild(message.guild).channels():
             return
         if not message.attachments:
-            escaped = common_filters.escape_spoilers(message.content)
+            escaped = discord.utils.escape_markdown(message.content)
             stripped = re.sub("\\Q|\\E", "", escaped)  
             uris = re.findall(
                 "(?=\S+youtube\.com|\S+youtu\.be\/|\S+tenor\.com|\S+\.mov|\S+\.jpg|\S+\.jpeg|\S+\.tiff|\S+\.gif|\S+\.bmp|\S+\.mp4|\S+\.webm|\S+\.png|)((?<!\S)(((f|ht){1}tp[s]?:\/\/+|(?<!\S)www\.)[-a-zA-Z0-9@:%_\+.~#?&|\/\/=]+))",
