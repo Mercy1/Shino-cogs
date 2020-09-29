e64d357f39cf34b8d70d536b978a5791f29a3b3e
DannyDB5544 <59149717+DannyDB5544@users.noreply.github.com>
2020-09-29 22:14:48 +0100

    Update gallery.py

--- gallery/gallery.py
+++ gallery/gallery.py
@@ -113,7 +113,7 @@ class Gallery(commands.Cog):
             await message.channel.send(f"{stripped}")
             uris = re.findall(
                 "(?=\S+youtube\.com|\S+youtu\.be\/|\S+tenor\.com|\S+\.mov|\S+\.jpg|\S+\.jpeg|\S+\.tiff|\S+\.gif|\S+\.bmp|\S+\.mp4|\S+\.webm|\S+\.png|)((?<!\S)(((f|ht){1}tp[s]?:\/\/+|(?<!\S)www\.)[-a-zA-Z0-9@:%_\+.~#?&|\/\/=]+))",
-                dab,
+                stripped,
             )
             if len(uris) == 1:
                 uri = "".join(uris)
