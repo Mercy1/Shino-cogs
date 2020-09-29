6bc3eefb2227fe8dc5c7c3153254486dffcc12b2
DannyDB5544 <59149717+DannyDB5544@users.noreply.github.com>
2020-09-29 22:35:45 +0100

    Update gallery.py

--- gallery/gallery.py
+++ gallery/gallery.py
@@ -108,8 +108,9 @@ class Gallery(commands.Cog):
             return
         if not message.attachments:
             escaped = discord.utils.escape_markdown(message.content)
-            stripped = escaped.lstrip("||")
-            print(stripped)
+            stripped = escaped.lstrip("\\|")
+            stripped2 = stripped.rstrip("|)
+            print(stripped2)
             uris = re.findall(
                 "(?=\S+youtube\.com|\S+youtu\.be\/|\S+tenor\.com|\S+\.mov|\S+\.jpg|\S+\.jpeg|\S+\.tiff|\S+\.gif|\S+\.bmp|\S+\.mp4|\S+\.webm|\S+\.png|)((?<!\S)(((f|ht){1}tp[s]?:\/\/+|(?<!\S)www\.)[-a-zA-Z0-9@:%_\+.~#?&|\/\/=]+))",
                 stripped,
